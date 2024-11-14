3295d0dd7f9dfcfd582c77aa08f669023e9a4786
CRTXScyNet <CRTXScyNet@gmail.com>
2024-11-14 20:06:16 +0500

    first hv is done

--- src/channel.py
+++ src/channel.py
@@ -1,12 +1,21 @@
+import json
+import os
 
+from googleapiclient.discovery import build
+
+
+
+youtube_api_key = os.getenv('YOUTUBE_API_KEY')
 
 class Channel:
     """Класс для ютуб-канала"""
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self.youtube = build('youtube','v3',developerKey=os.getenv('YOUTUBE_API_KEY'))
+        self.channel_id = channel_id
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+        channel = self.youtube.channels().list(id=self.channel_id,part='snippet,statistics').execute()
+        print(json.dumps(channel,indent=2,ensure_ascii=False))
